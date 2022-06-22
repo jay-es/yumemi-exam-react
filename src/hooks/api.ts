2579a2a039c4deaf4ccdbaa2862443d2bd13515c
Jun Shindo <46585162+jay-es@users.noreply.github.com>
2022-06-22 21:38:15 +0900

    API_ROOT を変数化

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -3,25 +3,24 @@ import { useQuery } from 'react-query'
 import { usePopulation, useSetPopulation } from '~/states/population'
 import { Prefecture, YearValue } from '~/types'
 
+const API_ROOT = 'https://opendata.resas-portal.go.jp/api/v1'
+
 const headers = {
   'X-API-KEY': import.meta.env.VITE_API_KEY,
 }
 
 export const usePrefectures = () =>
   useQuery<Prefecture[], Error>('prefectures', () =>
-    fetch('https://opendata.resas-portal.go.jp/api/v1/prefectures', {
-      headers,
-    })
+    fetch(`${API_ROOT}/prefectures`, { headers })
       .then((res) => res.json())
       .then((res) => res.result)
   )
 
 /** 人口構成 API から「総人口」部分のデータを取得 */
 const fetchPopulation = (prefCode: number): Promise<YearValue[]> =>
-  fetch(
-    `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}`,
-    { headers }
-  )
+  fetch(`${API_ROOT}/population/composition/perYear?prefCode=${prefCode}`, {
+    headers,
+  })
     .then((res) => res.json())
     .then((res) => res.result.data[0].data)
 
